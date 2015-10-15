1b24979b9d808b8eeaead6819b84e597d453c90b
abdousaanyani@gmail.com <abdousaanyani@yahoo.fr>
2015-10-15 14:17:37 -0700

    first code

---
+++ src/travail.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+public class travail {
+	public static void main(String[] args) {
+				try
+				{
+					Scanner kb = new Scanner(System.in);	
+			System.out.println("choose one of the option,  (1-3)"
+			+ "\n\t1.) ali" + "\n\t2.) Farid" + "\n\t3.) kevin");
+				byte ch = kb.nextByte();
+			/*
+			 if, else If, and else would be there
+			 */
+					}catch(Exception e){
+			System.out.println("INVALID");
+		}
+	}
+
+}
